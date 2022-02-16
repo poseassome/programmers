ffa817bbfc8840c66e635155fc8918e503e93517
jiyoung <higomn05@naver.com>
2022-02-17 01:06:00 +0900

    add solving

---
+++ Level_1/시저암호/solution.js
@@ -0,0 +1,6 @@
+function solution(s, n) {
+  return s.split("").map(e => {
+    if (" " == e) return e;
+    return e.toUpperCase().charCodeAt(0) + n > 90 ? String.fromCharCode(e.charCodeAt(0) + n - 26) : String.fromCharCode(e.charCodeAt(0) + n);
+  }).join("");
+}
