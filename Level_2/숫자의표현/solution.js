3f727bac10d1e720fdc24cc815d6c93cc44dd984
jiyoung <higomn05@naver.com>
2022-03-14 04:06:20 +0900

    add solving lv2 숫자의표현

---
+++ Level_2/숫자의표현/solution.js
@@ -0,0 +1,12 @@
+function solution(n) {
+  var answer = 0;
+  for (let i = 1; i <= n; i++) {
+    let sum = 0;
+    for (let j = i; j <= n; j++) {
+      sum += j;
+      if (sum === n) answer++
+      else if (sum > n) break;
+    }
+  }
+  return answer;
+}
