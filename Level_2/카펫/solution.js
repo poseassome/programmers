5dc7220e0461efc70894c4842d7c98f44c35c62e
jiyoung <higomn05@naver.com>
2022-03-15 15:41:24 +0900

    add solving 카펫

---
+++ Level_2/카펫/solution.js
@@ -0,0 +1,11 @@
+function solution(brown, yellow) {
+  var answer = [];
+  let sum = brown + yellow;
+  for (let h = 3; h <= brown; h++) {
+    if (sum % h === 0) {
+      let w = sum / h;
+      if ((h - 2) * (w - 2) === yellow) answer = [h, w];
+    }
+  }
+  return answer;
+}
