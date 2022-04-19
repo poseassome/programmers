a9785562e8a0bcbd8c6e3a7b8e5effa6803e4299
jiyoung <higomn05@naver.com>
2022-04-19 23:59:10 +0900

    add solving lv3 2xn타일링

---
+++ Level_3/2xn타일링/solution.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  var answer = 0;
+  var dp = Array(n).fill(0)
+  dp[0] = 1;
+  dp[1] = 2;
+  for (var i = 2; i < n; i++) {
+    var a = dp[i - 1] + dp[i - 2];
+    dp[i] = a % 1000000007;
+  }
+  return dp[n - 1];
+}
